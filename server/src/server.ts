0804bbfc5ca7eb6e3b05ea7ae643314ff9f42dd5
dimaspaiva <dimasalpaiva@gmail.com>
2020-06-10 02:25:39 -0300

    database connection success

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,20 @@
 import express from 'express'
+import knex from './database/connection'
 
 const app = express()
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   return res.json({ message: 'Hello world!' })
 })
 
-app.listen(4040)
+app.post('/users', async (req, res) => {
+  const idList = await knex('users').insert(req.body)
+
+  return res.json({ idList })
+})
+
+app.listen(4040, () => {
+  console.log('[SRV] up...')
+})
