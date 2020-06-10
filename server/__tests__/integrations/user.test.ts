78211803708c422b88d2afbbe0f133e290cb9f66
dimaspaiva <dimasalpaiva@gmail.com>
2020-06-10 04:35:42 -0300

    user tests

--- server/__tests__/integrations/user.test.ts
+++ server/__tests__/integrations/user.test.ts
@@ -14,11 +14,54 @@ describe('startup', () => {
 
   it('should create an user', async () => {
     const response = await request(app).post('/users').send({
-      name: 'Dimaas',
+      name: 'Dimas',
       email: 'dimaspaiva@gmail.com',
       password: '123456',
     })
 
     expect(response.status).toBe(200)
+  })
+
+  it('should not create an user with already registered email', async () => {
+    await knex('users').insert({
+      name: 'Dimas Jose',
+      email: 'dimaspaiva@gmail.com',
+      password: '654321',
+    })
+
+    const response = await request(app).post('/users').send({
+      name: 'Dimas',
+      email: 'dimaspaiva@gmail.com',
+      password: '123456',
+    })
+
+    expect(response.status).toBe(400)
+  })
+
+  it('should not create an user without email', async () => {
+    const response = await request(app).post('/users').send({
+      name: 'Dimas',
+      password: '123456',
+    })
+
+    expect(response.status).toBe(400)
+  })
+
+  it('should not create an user without password', async () => {
+    const response = await request(app).post('/users').send({
+      name: 'Dimas',
+      email: 'dimaspaiva@gmail.com',
+    })
+
+    expect(response.status).toBe(400)
+  })
+
+  it('should not create an user without name', async () => {
+    const response = await request(app).post('/users').send({
+      email: 'dimaspaiva@gmail.com',
+      password: '123456',
+    })
+
+    expect(response.status).toBe(400)
   })
 })
--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -3,7 +3,19 @@ import knex from '../database/connection'
 
 class User {
   async create(req: Request, res: Response) {
-    const idList = await knex('users').insert(req.body)
+    if (!req.body.email || !req.body.name || !req.body.password) {
+      return res
+        .status(400)
+        .json({ message: 'One or more creating data is missing' })
+    }
+
+    const user = await knex('users').select('*').where('email', req.body.email)
+
+    if (user[0]) {
+      return res.status(400).json({ message: 'E-mail is already in use' })
+    }
+
+    await knex('users').insert(req.body)
 
     return res.json({ message: 'User created success' })
   }
