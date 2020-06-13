98a1b1877fe045fa4b5a0d550c04d16f18594ce2
dimaspaiva <dimasalpaiva@gmail.com>
2020-06-13 13:15:53 -0300

    home update

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,13 +1,102 @@
 import React from 'react'
-import { View, SafeAreaView, Text } from 'react-native'
+import { ScrollView, View, SafeAreaView, Text, Image } from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
+import { useNavigation } from '@react-navigation/native'
+
+import styles from './styles'
+import global from '../styles-global'
+import { TouchableOpacity, TextInput } from 'react-native-gesture-handler'
+import MainButton from '../../components/MainButton'
 
 const Home = () => {
+  const navigation = useNavigation()
+
+  const handleLogout = () => {
+    navigation.goBack()
+  }
+
+  const handleSelectRocket = () => {
+    navigation.navigate('Rockets')
+  }
+
   return (
-    <SafeAreaView
-      style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <View>
-        <Text style={{ color: '#eaeaea' }}>Home view</Text>
+    <SafeAreaView style={global.container}>
+      <View style={styles.header}>
+        <View style={styles.headerContainer}>
+          <Image
+            style={styles.headerLogo}
+            source={require('../../img/logo-min.png')}
+          />
+          <Text style={styles.headerSlogan}>
+            Flying with Comfort and Safety
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          onPress={() => handleLogout()}
+          style={styles.logoutContainer}>
+          <Icon name="power" color="#FF6666" size={30} />
+          <Text style={styles.logoutTitle}>Logout</Text>
+        </TouchableOpacity>
       </View>
+
+      <View style={styles.body}>
+        <Text style={global.pageTitle}>When you want to travel</Text>
+
+        <View style={styles.infosContainer}>
+          <Text style={styles.infosText}>t - 4: Chose when, date and time</Text>
+          <Text style={styles.infosText}>
+            t - 3: Choose your favorite companie
+          </Text>
+          <Text style={styles.infosText}>t - 2: Choose a nice rocket</Text>
+          <Text style={styles.infosText}>t - 1: Departure</Text>
+          <Text style={styles.infosText}>t - 0: Launch!</Text>
+        </View>
+
+        <TextInput
+          style={[global.input, { marginBottom: 30 }]}
+          placeholder="Flight date"
+        />
+        <TextInput
+          style={[global.input, { marginBottom: 30 }]}
+          placeholder="Flight time"
+        />
+
+        <ScrollView
+          style={styles.companiesList}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ padding: 0 }}>
+          <View style={styles.companieContainer}>
+            <View style={styles.companieLogo} />
+            <Text style={styles.companieName}>Company name</Text>
+          </View>
+          <View style={styles.companieContainer}>
+            <View style={styles.companieLogo} />
+            <Text style={styles.companieName}>Company name</Text>
+          </View>
+          <View style={styles.companieContainer}>
+            <View style={styles.companieLogo} />
+            <Text style={styles.companieName}>Company name</Text>
+          </View>
+          <View style={styles.companieContainer}>
+            <View style={styles.companieLogo} />
+            <Text style={styles.companieName}>Company name</Text>
+          </View>
+          <View style={styles.companieContainer}>
+            <View style={styles.companieLogo} />
+            <Text style={styles.companieName}>Company name</Text>
+          </View>
+        </ScrollView>
+      </View>
+
+      <MainButton
+        color="#9966FF"
+        darkColor="#7C48E4"
+        text="Select your Rocket!"
+        icon="navigation-2"
+        action={handleSelectRocket}
+      />
     </SafeAreaView>
   )
 }
--- mobile/src/pages/Home/styles.tsx
+++ mobile/src/pages/Home/styles.tsx
@@ -1,3 +1,86 @@
 import { StyleSheet } from 'react-native'
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: 300,
+  },
+
+  headerContainer: {
+    width: 230,
+    marginBottom: 21,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  headerLogo: {
+    width: 56,
+    height: 75,
+  },
+
+  headerSlogan: {
+    width: 150,
+    fontFamily: 'Evogria',
+    color: '#EAEAEA',
+    fontSize: 15,
+  },
+
+  logoutContainer: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 60,
+  },
+
+  logoutTitle: {
+    color: '#FF6666',
+    fontFamily: 'Evogria',
+    fontSize: 15,
+    textAlign: 'center',
+  },
+
+  body: {
+    width: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  infosContainer: {
+    width: 270,
+    marginTop: 9,
+    marginBottom: 21,
+  },
+
+  infosText: {
+    fontSize: 15,
+    fontFamily: 'Evogria',
+    color: '#949494',
+  },
+
+  companiesList: {
+    marginBottom: 40,
+  },
+
+  companieContainer: {
+    width: 75,
+    height: 99,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  companieLogo: {
+    width: 50,
+    height: 50,
+    borderRadius: 9,
+    backgroundColor: '#EAEAEA',
+  },
+
+  companieName: {
+    fontFamily: 'Evogria',
+    fontSize: 15,
+    color: '#EAEAEA',
+    textAlign: 'center',
+  },
+})
+
+export default styles
