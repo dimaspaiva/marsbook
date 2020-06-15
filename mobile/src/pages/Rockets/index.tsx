1c93bd719d832e8a3424baa66676fda4e66edf0a
dimaspaiva <dimasalpaiva@gmail.com>
2020-06-15 01:34:53 -0300

    select a rocket start

--- mobile/src/pages/Rockets/index.tsx
+++ mobile/src/pages/Rockets/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   TouchableOpacity,
   View,
@@ -7,7 +7,7 @@ import {
   ScrollView,
 } from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 
 import MainButton from '../../components/MainButton'
 
@@ -15,11 +15,37 @@ import styles from './styles'
 import global from '../styles-global'
 import Return from '../../components/ReturnButton'
 
+interface Params {
+  rockets: Rocket[]
+}
+
+interface Rocket {
+  id: number
+  model: string
+  seats: number
+  price: number
+  launch: string
+  company: number
+  rating: number
+}
+
 const Rockets = () => {
   const navigation = useNavigation()
+  const route = useRoute()
+  const routeParams = route.params as Params
+
+  const [rockets, setRockets] = useState<Rocket[]>([])
+
+  useEffect(() => {
+    setRockets(routeParams.rockets)
+  }, [])
 
   const handleMoveFinalize = () => {
     navigation.navigate('Purchase')
+  }
+
+  const formatPrice = (price: number) => {
+    const newPrice = price.toFixed(2)
   }
 
   return (
@@ -37,230 +63,36 @@ const Rockets = () => {
       <ScrollView
         style={styles.rocketsList}
         showsVerticalScrollIndicator={false}>
-        <View style={styles.rocketContainer}>
-          <View style={styles.rocketLogo} />
+        {rockets.map((rocket) => (
+          <View style={styles.rocketContainer}>
+            <View style={styles.rocketLogo} />
 
-          <View>
-            <View style={styles.rocketHeader}>
-              <Text style={styles.rocketName}>Spaceship name</Text>
+            <View>
+              <View style={styles.rocketHeader}>
+                <Text style={styles.rocketName}>{rocket.model}</Text>
 
-              <View style={styles.rocketTicketContainer}>
-                <Text style={styles.rocketTickets}>25</Text>
-                <Icon name="users" color="#9966FF" size={24} />
+                <View style={styles.rocketTicketContainer}>
+                  <Text style={styles.rocketTickets}>{rocket.seats}</Text>
+                  <Icon name="users" color="#9966FF" size={24} />
+                </View>
               </View>
-            </View>
 
-            <Text style={styles.rocketInfo}>AMAZING NON SENSE TEXT</Text>
+              <Text style={styles.rocketInfo}>
+                Amazing motivation flight text
+              </Text>
 
-            <View style={styles.rocketFooter}>
-              <View style={styles.rocketNoteContainer}>
-                <Icon name="award" color="#9966FF" size={24} />
-                <Text style={styles.rocketNote}>5.0</Text>
+              <View style={styles.rocketFooter}>
+                <View style={styles.rocketNoteContainer}>
+                  <Icon name="award" color="#9966FF" size={24} />
+                  <Text style={styles.rocketNote}>{rocket.rating}</Text>
+                </View>
+                <Text style={styles.rocketPrice}>
+                  U$ {rocket.price.toFixed(2)}
+                </Text>
               </View>
-              <Text style={styles.rocketPrice}>R$ 15,000.00</Text>
             </View>
           </View>
-        </View>
-
-        <View style={styles.rocketContainer}>
-          <View style={styles.rocketLogo} />
-
-          <View>
-            <View style={styles.rocketHeader}>
-              <Text style={styles.rocketName}>Spaceship name</Text>
-
-              <View style={styles.rocketTicketContainer}>
-                <Text style={styles.rocketTickets}>25</Text>
-                <Icon name="users" color="#9966FF" size={24} />
-              </View>
-            </View>
-
-            <Text style={styles.rocketInfo}>AMAZING NON SENSE TEXT</Text>
-
-            <View style={styles.rocketFooter}>
-              <View style={styles.rocketNoteContainer}>
-                <Icon name="award" color="#9966FF" size={24} />
-                <Text style={styles.rocketNote}>5.0</Text>
-              </View>
-              <Text style={styles.rocketPrice}>R$ 15,000.00</Text>
-            </View>
-          </View>
-        </View>
-
-        <View style={styles.rocketContainer}>
-          <View style={styles.rocketLogo} />
-
-          <View>
-            <View style={styles.rocketHeader}>
-              <Text style={styles.rocketName}>Spaceship name</Text>
-
-              <View style={styles.rocketTicketContainer}>
-                <Text style={styles.rocketTickets}>25</Text>
-                <Icon name="users" color="#9966FF" size={24} />
-              </View>
-            </View>
-
-            <Text style={styles.rocketInfo}>AMAZING NON SENSE TEXT</Text>
-
-            <View style={styles.rocketFooter}>
-              <View style={styles.rocketNoteContainer}>
-                <Icon name="award" color="#9966FF" size={24} />
-                <Text style={styles.rocketNote}>5.0</Text>
-              </View>
-              <Text style={styles.rocketPrice}>R$ 15,000.00</Text>
-            </View>
-          </View>
-        </View>
-
-        <View style={styles.rocketContainer}>
-          <View style={styles.rocketLogo} />
-
-          <View>
-            <View style={styles.rocketHeader}>
-              <Text style={styles.rocketName}>Spaceship name</Text>
-
-              <View style={styles.rocketTicketContainer}>
-                <Text style={styles.rocketTickets}>25</Text>
-                <Icon name="users" color="#9966FF" size={24} />
-              </View>
-            </View>
-
-            <Text style={styles.rocketInfo}>AMAZING NON SENSE TEXT</Text>
-
-            <View style={styles.rocketFooter}>
-              <View style={styles.rocketNoteContainer}>
-                <Icon name="award" color="#9966FF" size={24} />
-                <Text style={styles.rocketNote}>5.0</Text>
-              </View>
-              <Text style={styles.rocketPrice}>R$ 15,000.00</Text>
-            </View>
-          </View>
-        </View>
-
-        <View style={styles.rocketContainer}>
-          <View style={styles.rocketLogo} />
-
-          <View>
-            <View style={styles.rocketHeader}>
-              <Text style={styles.rocketName}>Spaceship name</Text>
-
-              <View style={styles.rocketTicketContainer}>
-                <Text style={styles.rocketTickets}>25</Text>
-                <Icon name="users" color="#9966FF" size={24} />
-              </View>
-            </View>
-
-            <Text style={styles.rocketInfo}>AMAZING NON SENSE TEXT</Text>
-
-            <View style={styles.rocketFooter}>
-              <View style={styles.rocketNoteContainer}>
-                <Icon name="award" color="#9966FF" size={24} />
-                <Text style={styles.rocketNote}>5.0</Text>
-              </View>
-              <Text style={styles.rocketPrice}>R$ 15,000.00</Text>
-            </View>
-          </View>
-        </View>
-
-        <View style={styles.rocketContainer}>
-          <View style={styles.rocketLogo} />
-
-          <View>
-            <View style={styles.rocketHeader}>
-              <Text style={styles.rocketName}>Spaceship name</Text>
-
-              <View style={styles.rocketTicketContainer}>
-                <Text style={styles.rocketTickets}>25</Text>
-                <Icon name="users" color="#9966FF" size={24} />
-              </View>
-            </View>
-
-            <Text style={styles.rocketInfo}>AMAZING NON SENSE TEXT</Text>
-
-            <View style={styles.rocketFooter}>
-              <View style={styles.rocketNoteContainer}>
-                <Icon name="award" color="#9966FF" size={24} />
-                <Text style={styles.rocketNote}>5.0</Text>
-              </View>
-              <Text style={styles.rocketPrice}>R$ 15,000.00</Text>
-            </View>
-          </View>
-        </View>
-
-        <View style={styles.rocketContainer}>
-          <View style={styles.rocketLogo} />
-
-          <View>
-            <View style={styles.rocketHeader}>
-              <Text style={styles.rocketName}>Spaceship name</Text>
-
-              <View style={styles.rocketTicketContainer}>
-                <Text style={styles.rocketTickets}>25</Text>
-                <Icon name="users" color="#9966FF" size={24} />
-              </View>
-            </View>
-
-            <Text style={styles.rocketInfo}>AMAZING NON SENSE TEXT</Text>
-
-            <View style={styles.rocketFooter}>
-              <View style={styles.rocketNoteContainer}>
-                <Icon name="award" color="#9966FF" size={24} />
-                <Text style={styles.rocketNote}>5.0</Text>
-              </View>
-              <Text style={styles.rocketPrice}>R$ 15,000.00</Text>
-            </View>
-          </View>
-        </View>
-
-        <View style={styles.rocketContainer}>
-          <View style={styles.rocketLogo} />
-
-          <View>
-            <View style={styles.rocketHeader}>
-              <Text style={styles.rocketName}>Spaceship name</Text>
-
-              <View style={styles.rocketTicketContainer}>
-                <Text style={styles.rocketTickets}>25</Text>
-                <Icon name="users" color="#9966FF" size={24} />
-              </View>
-            </View>
-
-            <Text style={styles.rocketInfo}>AMAZING NON SENSE TEXT</Text>
-
-            <View style={styles.rocketFooter}>
-              <View style={styles.rocketNoteContainer}>
-                <Icon name="award" color="#9966FF" size={24} />
-                <Text style={styles.rocketNote}>5.0</Text>
-              </View>
-              <Text style={styles.rocketPrice}>R$ 15,000.00</Text>
-            </View>
-          </View>
-        </View>
-
-        <View style={styles.rocketContainer}>
-          <View style={styles.rocketLogo} />
-
-          <View>
-            <View style={styles.rocketHeader}>
-              <Text style={styles.rocketName}>Spaceship name</Text>
-
-              <View style={styles.rocketTicketContainer}>
-                <Text style={styles.rocketTickets}>25</Text>
-                <Icon name="users" color="#9966FF" size={24} />
-              </View>
-            </View>
-
-            <Text style={styles.rocketInfo}>AMAZING NON SENSE TEXT</Text>
-
-            <View style={styles.rocketFooter}>
-              <View style={styles.rocketNoteContainer}>
-                <Icon name="award" color="#9966FF" size={24} />
-                <Text style={styles.rocketNote}>5.0</Text>
-              </View>
-              <Text style={styles.rocketPrice}>R$ 15,000.00</Text>
-            </View>
-          </View>
-        </View>
+        ))}
       </ScrollView>
 
       <MainButton
